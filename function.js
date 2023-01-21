f8a8243142d67b832825fab96f5c2e27dad1e300
itsjordanmuller <itsjordanmuller@gmail.com>
2023-01-20 21:24:02 -0800

    Create Multiplication Function

--- function.js
+++ function.js
@@ -8,3 +8,8 @@ function add7(input1) {
   return input1 + 7
 }
 console.log(add7(1))
+// Take 2 Numbers and Return the Product (Multiply Them)
+function multiply(input2, input3) {
+  return input2 * input3
+}
+console.log(multiply(10, 5))
