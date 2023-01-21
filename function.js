eca4911422310d619aadbf9a16fc2866edfc4572
itsjordanmuller <itsjordanmuller@gmail.com>
2023-01-20 21:21:20 -0800

    Create Function that Adds 7 to Input

--- function.js
+++ function.js
@@ -3,3 +3,8 @@ function favoriteAnimal(animal) {
     return animal + " are my favorite animal!"
 }
 console.log(favoriteAnimal('Cats'))
+// Add 7 to Input
+function add7(input1) {
+  return input1 + 7
+}
+console.log(add7(1))
